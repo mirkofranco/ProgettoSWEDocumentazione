ad4b11649015de340c5126e5b95055af93d7f024
sgt390 <35218358+sgt390@users.noreply.github.com>
2019-01-14 22:32:02 +0100

    Update grammar.sh

--- grammar.sh
+++ grammar.sh
@@ -8,7 +8,7 @@ BLUE='\033[0;36m'
 NC='\033[0m' # No Color
 
 #MARKDOWN_FILES_CHANGED=`(git diff --name-only $TRAVIS_COMMIT_RANGE || true) | grep .md`
-MARKDOWN_FILES_CHANGED=` git ls-files | grep "md"`
+MARKDOWN_FILES_CHANGED=` git ls-files | grep "tex"`
 echo $MARKDOWN_FILES_CHANGED
 
 if [ -z "$MARKDOWN_FILES_CHANGED" ]
