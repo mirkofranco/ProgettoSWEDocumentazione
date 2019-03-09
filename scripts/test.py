4b03ea8ff065f65ddd4b8dea3702b466faee7f98
sgt390 <stefano.sgt39@gmail.com>
2019-03-09 16:18:14 +0100

    should work

---
+++ scripts/test.py
@@ -0,0 +1,27 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import os
+
+
+pdf_files = ['../Esterni/PianoDiQualifica/PianoDiQualifica.pdf',
+    '../Esterni/PianoDiProgetto/PianoDiProgetto.pdf',
+    '../Esterni/AnalisiDeiRequisiti/AnalisiDeiRequisiti.pdf',
+    '../Interni/NormeDiProgetto/NormeDiProgetto.pdf']
+
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+sheet = client.open('ZeroSevenDocuments').sheet1
+
+gulpease_arr = []
+for file in pdf_files:
+    if os.path.isfile(file):
+        gulpease_arr.append(file)
+    else:
+        gulpease_arr.append(-1)
+
+sheet.insert_row(gulpease_arr, 2)
+
+records = sheet.get_all_records()
+print(records)
