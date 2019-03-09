696bd340f9531b9351e5cf55d8c3abe035b17964
sgt390 <stefano.sgt39@gmail.com>
2019-03-09 14:59:56 +0100

    gulpease + g drive

--- scripts/gulpease.py
+++ scripts/gulpease.py
@@ -4,6 +4,12 @@
 #import sys
 import textract
 
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pprint
+
+import os
+
 import re
 def gulpease(pdf_file):
     testo = textract.process(pdf_file, method='pdftotext')
@@ -12,26 +18,10 @@ def gulpease(pdf_file):
     lettere = len(re.findall(r'\w', testo))
     punti = len(re.findall(r'[.]+\s', testo)) + len(re.findall(r'[;]+\s', testo)) - len(re.findall(r'[.]+\s+[.]', testo))
     indiceG=89+((300*punti)-(10*lettere))/parole
-    result += pdf_file
-    result += "numero di parole presenti nei doc :   " + str(parole) + '\n'
-    result += "numero di lettere presenti nei doc :  " + str(lettere) + '\n'
-    result += "numero di frasi presenti nei doc :    " + str(punti) + '\n'
     if parole!=0:
         if indiceG>100:
             indiceG=100
 
-        result += "indice di Gulpease restrittivo : " + str(indiceG) + '\n'
-        punti = len(re.findall('[.]', testo)) + len(re.findall('[;]', testo))
-        indiceG = 89 + ((300 * punti) - (10 * lettere)) / parole
-        if indiceG>100:
-            indiceG=100
-
-        result += "indice di Gulpease non restrittivo : " + str(indiceG) + '\n'
-        result += "Nel primo indice non viene considerato delimitatore di frase:" + '\n'
-        result += "- la punteggiatura spazio punteggiatura come delimitatore di frasi" + '\n'
-        result += "- la punteggiatura che non e' seguita da un carattere di spaziatura" + '\n'
-    else:
-        result += "Errore nel calcolo dell'indice Gulpease" + '\n'
     return result
 
 
@@ -40,7 +30,22 @@ pdf_files = ['../Esterni/PianoDiQualifica/PianoDiQualifica.pdf',
     '../Esterni/AnalisiDeiRequisiti/AnalisiDeiRequisiti.pdf',
     '../Interni/NormeDiProgetto/NormeDiProgetto.pdf']
 
-import os.path
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+sheet = client.open('ZeroSevenDocuments').sheet1
+
+pp = pprint.PrettyPrinter()
+result = sheet.get_all_records()
+pp.pprint(result)
+
+
+gulpease_arr = []
 for file in pdf_files:
     if os.path.isfile(file):
-        print(gulpease(file))
+        gulpease_arr.append(gulpease(file))
+    else:
+        gulpease_arr.append(-1)
+
+sheet.insert_row(gulpease_arr, 1)
--- scripts/test.py
+++
@@ -1,8 +0,0 @@
-from tika import parser
-
-testo = "ciao, sono un tavolo. Sono fatto di legno. ciao, sono un tavolo; Sono fatto di legno. ciao, sono un tavolo. Sono fatto di legno. ciao, sono un tavolo. Sono fatto di legno."
-import re
-
-punti = len(re.findall(r'[.]+\s', testo)) + len(re.findall(r'[;]+\s', testo)) - len(re.findall(r'[.]+\s+[.]', testo))
-
-print(punti)
